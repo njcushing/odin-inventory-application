cf373bf6ed89bf7e633860ec27f33a970ca54a57
njcushing <129661299+njcushing@users.noreply.github.com>
2023-11-07 20:41:43 +0000

    Add /catalog router into middleware chain in app.js

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog");
 
 var app = express();
 
@@ -34,6 +35,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
